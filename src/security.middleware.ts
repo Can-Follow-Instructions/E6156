50489cc962494eac42fe993c6072e42513a95f5e
zerolfx <zerolfx0@gmail.com>
2021-11-14 01:08:28 -0500

    fix: security middleware

--- src/security.middleware.ts
+++ src/security.middleware.ts
@@ -4,6 +4,8 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class SecurityMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.redirect('google/redirect');
+    // @ts-ignore
+    if (req.session.user) next();
+    else res.redirect('google/redirect');
   }
 }
