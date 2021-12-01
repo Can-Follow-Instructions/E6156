c4259bfb1da4b098b307df24025a0607691b57d1
zerolfx <zerolfx0@gmail.com>
2021-12-01 02:27:10 -0500

    add: 3rd party API

--- src/security.middleware.ts
+++ src/security.middleware.ts
@@ -1,11 +1,23 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import axios from 'axios';
 
 @Injectable()
 export class SecurityMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  async use(req: Request, res: Response, next: NextFunction) {
     // @ts-ignore
-    if (req.session.user) next();
-    else res.redirect('google/redirect');
+    const ip_addr = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    if (req.session.ip && req.session.ip !== ip_addr) {
+      const r1 = await axios.get('http://ip-api.com/json/' + req.session.ip);
+      const r2 = await axios.get('http://ip-api.com/json/' + ip_addr);
+      if (r1.data.status !== 'fail' && r2.data.status !== 'fail' && r1.data.city !== r2.data.city) {
+        req.session.user = null;
+      }
+    }
+    if (req.session.user) {
+      next();
+      return;
+    }
+    res.redirect('google/redirect');
   }
 }
