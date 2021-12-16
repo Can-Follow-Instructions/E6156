22393318e13c307f746b7daa0248ad6d9a47cd79
zerolfx <zerolfx0@gmail.com>
2021-12-16 18:27:24 -0500

    remove CORS for deploy

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors({ credentials: true, origin: 'http://f.1oop.ml:3001' });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
